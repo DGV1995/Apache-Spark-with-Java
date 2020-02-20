c8386dc057c711bae2d651da0b190621a3af4987
Diego García-Viana <diego.garcia-viana@thermofisher.com>
2020-02-20 12:26:19 +0100

    Project2 - Different kind of schemas

--- project2/src/main/java/com/diegogv/spark/Application.java
+++ project2/src/main/java/com/diegogv/spark/Application.java
@@ -1,12 +1,18 @@
 package com.diegogv.spark;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Level;
+
 public class Application {
     public static void main(String args[]) {
-        // InferCSVSchema csvParser = new InferCSVSchema();
-        // csvParser.printSchema();
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        Logger.getLogger("akka").setLevel(Level.ERROR);
 
-        // DefineCSVSchema definedSchemaParser = new DefineCSVSchema();
-        // definedSchemaParser.printDefinedSchema();
+        InferCSVSchema csvParser = new InferCSVSchema();
+        csvParser.printSchema();
+
+        DefineCSVSchema definedSchemaParser = new DefineCSVSchema();
+        definedSchemaParser.printDefinedSchema();
 
         JSONLinesParser jsonParser = new JSONLinesParser();
         jsonParser.parseJsonLines();
--- project2/src/main/java/com/diegogv/spark/JSONLinesParser.java
+++ project2/src/main/java/com/diegogv/spark/JSONLinesParser.java
@@ -3,9 +3,6 @@ package com.diegogv.spark;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 
 public class JSONLinesParser {
     public void parseJsonLines() {
