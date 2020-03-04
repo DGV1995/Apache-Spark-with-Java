4c88ef088e0259b4109715508c7c308cca1fd661
Diego García-Viana <diego.garcia-viana@thermofisher.com>
2020-03-04 09:35:52 +0100

    Map and Reduce functions

--- project4/src/main/java/com/diegogv/spark/ArrayToDataset.java
+++ project4/src/main/java/com/diegogv/spark/ArrayToDataset.java
@@ -1,10 +1,13 @@
 package com.diegogv.spark;
 
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.ReduceFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +23,7 @@ public class ArrayToDataset {
 
         Dataset<String> ds = spark.createDataset(data, Encoders.STRING());
         // Dataset to Dataframe
-        Dataset<Row> df = ds.toDF();
+        /*Dataset<Row> df = ds.toDF();
         // Dataframe to Dataset
         ds = df.as(Encoders.STRING());
 
@@ -28,6 +31,14 @@ public class ArrayToDataset {
         ds.printSchema();
 
         Dataset<Row> df2 = df.groupBy("value").count();
-        df2.show();
+        df2.show();*/
+
+        // Map function
+        Dataset<String> ds2 = ds.map((MapFunction<String, String>) row -> "word: " + row, Encoders.STRING());
+        ds2.show();
+
+        // Reduce function
+        String finalValue = ds.reduce((ReduceFunction<String>) (row1, row2) -> row1 + row2);
+        System.out.println(finalValue);
     }
 }
